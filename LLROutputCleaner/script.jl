be9d62c54e321e550f63983e9a843c56f9df7916
Fabian Zierler <fabian.zierler@a1.net>
2024-11-12 13:46:47 +0000

    create cleaned directories

--- LLROutputCleaner/script.jl
+++ LLROutputCleaner/script.jl
@@ -28,28 +28,6 @@ function check_llr_files(start, finish, nlines)
     end
     return true
 end
-function assert_llr_files(file)
-    start, finish, nlines = llr_start_and_end(file)
-    @assert first(start)  == 1
-    @assert last(finish)  == nlines
-    @assert length(start) == length(finish)
-    for i in 1:length(start)-1
-        @assert  start[i+1] - 1 == finish[i]
-    end
-end
-function find_malformed_files_in_dir(dir)
-    paths = AbstractString[]
-    for (root,dirs,files) in walkdir(dir)
-        if "out_0" ∈ files
-            file = joinpath(root,"out_0")
-            healthy = check_llr_files(file)
-            if !healthy
-                push!(paths,file)
-            end
-        end
-    end
-    return paths
-end
 function find_healthy_ranges(start, finish)
     # start with the first start of a run
     ind_s0 = 1
@@ -71,23 +49,34 @@ function find_healthy_ranges(start, finish)
     end
     return ranges
 end
-function find_malformed_files_and_healthy_ranges(dir)
+function find_malformed_files_and_healthy_ranges(dir,newdir)
     paths_and_ranges = Dict()
     for (root,dirs,files) in walkdir(dir)
         if "out_0" ∈ files
             file = joinpath(root,"out_0")
             start, finish, nlines = llr_start_and_end(file)
             healthy = check_llr_files(start, finish, nlines)
-            if !healthy
+            newfile = joinpath(newdir,relpath(file,dir))
+            ispath(dirname(newfile)) || mkpath(dirname(newfile))
+            if healthy
+                cp(file,newfile)
+            else
                 ranges = find_healthy_ranges(start, finish)
                 paths_and_ranges[file] = ranges
+                io = open(newfile,"w")
+                old_file = readlines(file)
+                for r in ranges
+                    for l in r
+                       write(io,old_file[l])
+                    end
+                end
+                close(io)
             end
         end
     end
     return paths_and_ranges
 end
 
-testfile = "/home/fabian/Documents/Physics/Data/DataCSD/CSD3/LLR_6x72_76/9/Rep_9/out_0"
-testdir = "/home/fabian/Documents/Physics/Data/DataCSD/CSD3/"
-
-dict = find_malformed_files_and_healthy_ranges(testdir)
+testdir = "/home/fabian/Documents/Physics/Data/DataCSD/Archives/full/"
+newdir  = "/home/fabian/Documents/Physics/Data/DataCSD/Archives/cleaned/"
+dict = find_malformed_files_and_healthy_ranges(testdir,newdir)
