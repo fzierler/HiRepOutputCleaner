e19d501ff58e9c6783ca47e450ec5be7aabc37e5
Fabian Zierler <fabian.zierler@a1.net>
2025-06-02 12:09:46 +0100

    add function for truncating llr runs

--- scriptLLR.jl
+++ scriptLLR.jl
@@ -1,7 +1,31 @@
 using Pkg; Pkg.activate(".")
 using HiRepOutputCleaner
 
-dir    = "/home/fabian/Documents/Physics/Data/DataSunbird/Sunbird/LLR/LLR_5x80_64/0/"
-newdir = "/home/fabian/Downloads/LLR_5x80_64/" 
+function truncate_llr_directory(dir,newdir,runs;dry_run=false)    
+    for (root,dirs,files) in walkdir(dir)
+        if "out_0" ∈ files
+
+            file = joinpath(root,"out_0")
+            start, finish, nlines = HiRepOutputCleaner.hirep_start_and_end(file)
+            healthy = HiRepOutputCleaner.check_hirep_file(start, finish, nlines)
+            ranges = HiRepOutputCleaner.find_healthy_ranges(start[1:runs], finish[1:runs]) 
+
+            @assert healthy
+            @show file
+            @show length(start), length(finish), length(ranges)
+
+            if !dry_run
+                newfile = joinpath(newdir,relpath(file,dir))
+                ispath(dirname(newfile)) || mkpath(dirname(newfile))
+                HiRepOutputCleaner.write_healthy_ranges(newfile,file,ranges)
+            end
+        end
+    end
+end
+
+dir    = "/home/fabian/Documents/Physics/Data/DataDiaL/LLR_DiaL3/LLR_5x80_64"
+newdir = "/home/fabian/Downloads/LLR_5x80_64/3/"
+truncdir = "/home/fabian/Downloads/LLR_5x80_64_trunc/3/"
 #clean_llr_directory(dir,newdir;checkpoint_pattern=nothing,last_ranges=nothing)
-HiRepOutputCleaner.force_clean_llr_directory(dir,newdir;checkpoint_pattern=nothing,last_ranges=nothing)
+#HiRepOutputCleaner.force_clean_llr_directory(dir,newdir;checkpoint_pattern=nothing,last_ranges=nothing)
+truncate_llr_directory(newdir,truncdir,16,dry_run=false)
