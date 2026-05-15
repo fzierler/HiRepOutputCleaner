39801e37ac67d721b342f1c24875cd4862f79215
Fabian Zierler <fabian.zierler@a1.net>
2026-05-15 15:21:19 +0200

    fix filenames

--- merge_llr.jl
+++ merge_llr.jl
@@ -38,7 +38,7 @@ function replica_id_from_file(file)
     return only(unique(replica_ids))
 end
 
-function main(dir,newdir)
+function main(dir,newdir;dry_run=true)
 
     # find all directories in overarching directory
     folders = filter(isdir,readdir(dir,join=true))
@@ -90,10 +90,12 @@ function main(dir,newdir)
         @assert missing_replica_id_alt == missing_replica
 
         # make a copy of the old data which will be used morked on
-        cp(joinpath(dir,repeat),joinpath(newdir,repeat))
+        ispath(newdir) || mkpath(newdir)
+        cp(joinpath(dir,basename(repeat)),joinpath(newdir,basename(repeat)))
+        missing_replica_file_new = replace(missing_replica_file,dir=>newdir)
 
         # Append the file 
-        io = open(missing_replica_file,"a")
+        io = open(missing_replica_file_new,"a")
         for l in eachline(file2append)
             write(io,l)
         end
@@ -106,3 +108,7 @@ end
 dir = "/home/fabian/Documents/Physics/Data/DataMareNostrum/LLR_SU4/LLR_su4_7x40_84_Run_3"
 newdir = "/home/fabian/Downloads/LLR_su4_7x40_84_Run_3_cleaned"
 main(dir, newdir)
+
+#dir = "/home/fabian/Documents/Physics/Data/DataMareNostrum/LLR_SU4/LLR_su4_5x32_84_Run_1"
+#newdir = "/home/fabian/Downloads/LLR_su4_5x32_84_Run_1_cleaned"
+#main(dir, newdir)
